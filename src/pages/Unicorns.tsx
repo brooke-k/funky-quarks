8e5e17fdf844984310a4ab509427bbe6e74cd3fa
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-01-17 14:25:21 -0700

    feat: Every time Unicorn page is open, a new random unicorn image is shown

--- src/pages/Unicorns.tsx
+++ src/pages/Unicorns.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "../theme/variables.scss";
 
+function randomLetter() {
+  if (Math.floor(Math.random()) == 0) {
+    return String.fromCharCode(Math.floor(Math.random() * (90 - 65) + 65));
+  } else {
+    return String.fromCharCode(Math.floor(Math.random() * (122 - 97) + 97));
+  }
+  const ascciVal = Math.random() * (90 - 65) + 65;
+  return String.fromCharCode(ascciVal);
+}
+
 const Home: React.FC = () => {
-  return <p className="primaryText unicornParagraph">z</p>;
+  return <p className="primaryText unicornParagraph">{randomLetter()}</p>;
 };
 
 export default Home;
