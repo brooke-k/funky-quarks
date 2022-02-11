584c4be5c057b5843dc321238c931db3dc501671
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-10 19:59:31 -0700

    feat: Fully updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export class Footer extends React.Component {
   render() {
     return (
       <div className="footerBox">
-        <p className="footerText">a small app by brooke kindleman</p>
+        <div className="footerText">a small app by brooke kindleman</div>
       </div>
     );
   }
